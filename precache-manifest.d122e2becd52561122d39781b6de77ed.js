d2ef83a4ddda9690a2eb36eacb21373b514549d2
mastelo <mirandarodriguezronald@gmail.com>
2020-08-30 17:43:48 -0400

    Updates

---
+++ precache-manifest.d122e2becd52561122d39781b6de77ed.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f0e0f0c650a354b0165a3194709321",
+    "url": "/nishop/index.html"
+  },
+  {
+    "revision": "ea9e775109e84044cab2",
+    "url": "/nishop/static/css/main.49b03a25.chunk.css"
+  },
+  {
+    "revision": "9926043f0b70238dfcb3",
+    "url": "/nishop/static/js/2.470e815b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nishop/static/js/2.470e815b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9e775109e84044cab2",
+    "url": "/nishop/static/js/main.22aceed6.chunk.js"
+  },
+  {
+    "revision": "65fa27473facedf64f14",
+    "url": "/nishop/static/js/runtime-main.152cebda.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/nishop/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "46f18f0985e33252c1afa5c8119cab4b",
+    "url": "/nishop/static/media/five.46f18f09.jfif"
+  },
+  {
+    "revision": "b80b019fd7dbfb04cc699257885d37b9",
+    "url": "/nishop/static/media/four.b80b019f.jfif"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/nishop/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "846db506d260c2ddae32c6098f4232ce",
+    "url": "/nishop/static/media/three.846db506.png"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/nishop/static/media/times-solid.2c5552a1.svg"
+  },
+  {
+    "revision": "5ba362562e9e847f34e9586a6ba57593",
+    "url": "/nishop/static/media/two.5ba36256.jfif"
+  }
+]);
