20e845696bdddd726d87c73d17f979cbaeb6ac10
mastelo <mirandarodriguezronald@gmail.com>
2020-09-03 14:36:11 -0400

    more responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -34,12 +34,18 @@ export class Header extends Component {
                 </div>
                 <nav>
                     <ul className={toggle ? "toggle" : ""}>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/product">Products</Link></li>                   
+                        <li><Link to="/"  onClick={this.menuToggle} >Home</Link></li>
+                        <li><Link to="/product"  onClick={this.menuToggle} >Products</Link></li>                   
                         <li className="close" onClick={this.menuToggle}>
                             <img src={Close} alt="" width="20"/>
                         </li>
+                        {/* <li>
+                            <Link className="counter" to="/cart"  onClick={this.menuToggle}>
+                                <b> Cart {cartCount > 0 ? cartCount : ""}  </b>
+                            </Link>
+                        </li> */}
                     </ul>
+
                     <div className="nav-cart">
                         <Link className="counter" to="/cart">
                             <img src={CartIcon} alt="" width="20" /> {cartCount > 0 ? cartCount : ""} 
